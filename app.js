c3fd804a679784c046d1d015f7b12eb6910671c1
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-17 15:48:57 +0300

    feat implement UI

---
+++ app.js
@@ -0,0 +1,9 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+  }
+}
